e841dd0836eed2db86688236feb4187e4365fc69
Serious Developer <vdenberg+github@gmail.com>
2015-08-14 23:01:17 +0200

    The application with HTML canvas arc() Method

--- README.md
+++ README.md
@@ -8,8 +8,7 @@ in libraries that are indispensible in being productive working with Scala.js.
 
 To get started, run `sbt ~fastOptJS` in this example project. This should
 download dependencies and prepare the relevant javascript files. If you open
-`localhost:12345/target/scala-2.11/classes/index-dev.html` in your browser, it will show you  animated soap bubbles. You can then
-edit the application and see the updates be sent live to the browser
+`localhost:12345/target/scala-2.11/classes/index-dev.html` in your browser, it will show you animated soap bubbles by clicking on the black background. You can edit the application and see the updates be sent live to the browser
 without needing to refresh the page.
 
 ## The optimized version
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -8,48 +8,28 @@ import scala.scalajs.js.annotation.JSExport
 
 case class Point(x: Double, y: Double) {
   def +(p: Point) = Point(x + p.x, y + p.y)
-  def -(p: Point) = Point(x - p.x, y - p.y)
-  def *(d: Double) = Point(x * d, y * d)
-  def /(d: Double) = Point(x / d, y / d)
-  def length = Math.sqrt(x * x + y * y)
 }
 
-case class Wave(pos: Point, var time: Int = 1) {
-  def wavefront(speed: Int): Seq[Point] = {
-    val (edge, dist) = (5, speed * time)
-
-    def inWavefront(p: Point): Boolean = {
-      val d = p.length
-      d < dist && d > (dist - edge)
-    }
-
-    for {
-      x <- -dist until dist
-      y <- -dist until dist
-      p = Point(x, y)
-      if inWavefront(p)
-    } yield p + pos
-  }
-}
+case class Wave(pos: Point, var time: Int = 1)
 
 @JSExport
 object ScalaJSExample {
 
   val canvas = dom.document.getElementById("canvas").asInstanceOf[html.Canvas]
-  val (ctx ,speed)= (canvas.getContext("2d"), 1)
+  val (ctx, speed) = (canvas.getContext("2d"), 1)
   var waves = mutable.Buffer[Wave]()
 
   @JSExport
   def doDynContent(): Unit = {
-    dom.console.log("Main started")
+    dom.console.log("doDynContent called")
 
     dom.document.onclick = { (e: dom.MouseEvent) =>
-      waves += Wave(Point(e.clientX.toInt, e.clientY.toInt))
+      waves.append(Wave(Point(e.clientX.toInt, e.clientY.toInt)))
     }
     dom.setInterval(() => {
       run()
       draw()
-    }, 30)
+    }, 50)
   }
 
   def run() {
@@ -63,16 +43,18 @@ object ScalaJSExample {
     })
   }
 
-  def draw() = { // drawing
+  def draw() = {
+    // drawing
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    def frontPoints = for {
-      wave <- waves
-       p <- wave.wavefront(speed)
-    } yield p
-
-    ctx.fillStyle = "magenta"
-    for (p <- frontPoints) ctx.fillRect(p.x - 10, p.y - 10, 1, 1)
+    ctx.strokeStyle = "magenta"
+    ctx.lineWidth = 5
+    waves.foreach { w => {
+      ctx.beginPath()
+      ctx.arc(w.pos.x, w.pos.y, speed * w.time, 0, 2 * math.Pi)
+    }
+      ctx.stroke()
+    }
   }
 }
