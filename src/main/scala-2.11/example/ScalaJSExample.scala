0e019a89bd42e274d01125656d98fc27d2e21bf4
SeriousSoftware <vdenberg+SeriousSoftware@gmail.com>
2016-08-31 14:46:08 +0200

    feat: JSExport s deleted, not necessary with JSApp trait.

--- src/main/scala-2.11/example/ScalaJSExample.scala
+++ src/main/scala-2.11/example/ScalaJSExample.scala
@@ -3,15 +3,12 @@ package example
 import org.scalajs.dom
 
 import scala.scalajs.js.JSApp
-import scala.scalajs.js.annotation.JSExport
 
-@JSExport
 object ScalaJSExample extends JSApp with Bubbles {
 
   val canvas = dom.document.getElementById("canvas main").asInstanceOf[dom.html.Canvas]
   val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
-  @JSExport
   def main() {
     dom.console.log("Main started with:", canvas.id)
 
