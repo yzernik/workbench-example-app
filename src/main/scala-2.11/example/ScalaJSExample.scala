7ec3986e7a23d1e9c249369248e05e106e01adcc
SeriousSoftware <vdenberg+SeriousSoftware@gmail.com>
2016-08-31 14:27:56 +0200

    feat: Some tidy up

--- src/main/scala-2.11/example/ScalaJSExample.scala
+++ src/main/scala-2.11/example/ScalaJSExample.scala
@@ -1,8 +1,6 @@
 package example
 
 import org.scalajs.dom
-import org.scalajs.dom._
-import org.scalajs.dom.html.Canvas
 
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
@@ -10,16 +8,16 @@ import scala.scalajs.js.annotation.JSExport
 @JSExport
 object ScalaJSExample extends JSApp with Bubbles {
 
-  val canvas = document.getElementById("canvas main").asInstanceOf[Canvas]
-  val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+  val canvas = dom.document.getElementById("canvas main").asInstanceOf[dom.html.Canvas]
+  val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
   @JSExport
   def main() {
-    console.log("Main started with", canvas.id)
+    dom.console.log("Main started with:", canvas.id)
 
     def run() = {
-      canvas.height = window.innerHeight.toInt
-      canvas.width = window.innerWidth.toInt
+      canvas.height = dom.window.innerHeight.toInt
+      canvas.width  = dom.window.innerWidth.toInt
 
       ctx.fillRect(0, 0, canvas.width, canvas.height)
 
